d3c16eb919f1cbef249b018d0e2e10b371a14a99
mucahitgunduz <a.gunduz2021@gtu.edu.tr>
2023-03-25 00:39:05 +0300

    path changed, in HW03 simple menu written.

---
+++ CSE_102_Homeworks(2023)/HW03/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int option;
+
+    printf("Welcome to the 2D puzzle game!");
+    printf("1. New Game\n");
+    printf("2. Help\n");
+    printf("3. Exit\n");
+    scanf("%d", &option);
+    switch (option)
+    {
+    case 1:
+        break;
+    
+    case 2:
+        break;
+
+    case 3:
+        break;
+
+    default:
+    }
+}
